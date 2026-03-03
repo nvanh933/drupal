5ab950df2fc1b9c67fb6856bb9667401fd5859d9
Anh Nguyen <nv.anh933@gmail.com>
2026-03-03 21:19:53 +0700

    Update some configuration

--- web/themes/vats/src/js/main.js
+++ web/themes/vats/src/js/main.js
@@ -3,29 +3,18 @@
 // ==============================
 import '../scss/main.scss';
 
-// Import Bootstrap CSS.
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-// Import Bootstrap JS bundle.
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-// Import Bootstrap Icons.
-import 'bootstrap-icons/font/bootstrap-icons.css';
 
 // ==============================
 // CORE (system-level logic)
 // ==============================
 import './core/drupal';
-import './core/global';
 
 
 // ==============================
 // COMPONENTS (UI-level logic)
 // ==============================
-import './components/hero';
 
 
 // ==============================
 // PAGES (page-specific logic)
 // ==============================
-import './pages/homepage';
