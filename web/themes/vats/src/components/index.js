814c786d3865e6d0920077117404496cb955d11c
Anh Nguyen <nv.anh933@gmail.com>
2026-03-05 22:49:55 +0700

    Create template for Testimonial, CTA components

--- web/themes/vats/src/components/index.js
+++ web/themes/vats/src/components/index.js
@@ -1,4 +1,7 @@
 import './content/content.scss';
+import './cta/cta.scss';
 import './features/features.scss';
 import './hero/hero.scss';
 import './listing-grid/listing-grid.scss';
+import './testimonials/testimonials.scss';
+import './testimonials/testimonials.js';
