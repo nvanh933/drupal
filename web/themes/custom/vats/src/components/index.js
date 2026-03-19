829560758b5473eb578b47ac2c55101750ae8d9f
Anh Nguyen <nv.anh933@gmail.com>
2026-03-19 14:06:24 +0700

    Update config

--- web/themes/custom/vats/src/components/index.js
+++ web/themes/custom/vats/src/components/index.js
@@ -2,13 +2,3 @@ import './layout/header/header.scss';
 import './layout/footer/footer.scss';
 
 import './ui/button.scss';
-
-import './paragraphs/bluecheese-cta/bluecheese-cta.scss';
-import './paragraphs/content/content.scss';
-import './paragraphs/cta/cta.scss';
-import './paragraphs/features/features.scss';
-import './paragraphs/hero/hero.scss';
-import './paragraphs/listing-grid/listing-grid.scss';
-import './paragraphs/testimonials/testimonials.scss';
-
-import './paragraphs/testimonials/testimonials.js';
